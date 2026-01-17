e1411daa556f49346f9fd1013a16f892d375fe99
Nghia Nguyen <minmin272610@gmail.com>
2026-01-17 17:33:08 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="w-fit mx-auto font-bold text-red-500 text-3xl">
+      This is test
+    </h1>
+  );
+}
+
+export default App;
